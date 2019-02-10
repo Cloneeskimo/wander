7a713da4659e845a1e6d94a2ed92bc7810c3c522
Jacob Oaks <jacoboaks.8@gmail.com>
2019-02-10 16:07:23 -0500

    b3 commented main.cpp

--- wander/main.cpp
+++ wander/main.cpp
@@ -3,25 +3,25 @@
 #include <string>
 #include <SFML/Window.hpp>
 
-static const int W_WIDTH = 800;
-static const int W_HEIGHT = 600;
-static const int A_BUILD = 2;
-static const std::string W_TITLE = "Wander b" + std::to_string(A_BUILD);
+static const int W_WIDTH = 800;   //defines width for window
+static const int W_HEIGHT = 600;  //defines height for window
+static const int A_BUILD = 3;     //records build no. for window
+static const std::string W_TITLE = "Wander b" + std::to_string(A_BUILD);  //defines title for window
 
 int main(int, char const**)
 {
-    sf::Window w(sf::VideoMode(W_WIDTH, W_HEIGHT), W_TITLE, sf::Style::Default);
+    sf::Window w(sf::VideoMode(W_WIDTH, W_HEIGHT), W_TITLE, sf::Style::Default); //create window
     
-    while (w.isOpen()) {
+    while (w.isOpen()) { //program loop
     
+        //check events
         sf::Event e;
-        
         while (w.pollEvent(e)) {
             
-            if (e.type == sf::Event::Closed)
+            if (e.type == sf::Event::Closed) //window close event
                 w.close();
         }
     }
     
-    return 0;
+    return 0; //return successful run
 }
