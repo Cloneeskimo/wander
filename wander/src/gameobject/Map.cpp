a81a2422e9bc7d834942b16d26f0fe074d44ec1a
Jacob Oaks <jacoboaks.8@gmail.com>
2019-03-24 23:04:07 -0400

    b75 added background layer to map loading, added random tile choosing from a list of possible choices given in the map file

--- wander/src/gameobject/Map.cpp
+++ wander/src/gameobject/Map.cpp
@@ -20,39 +20,28 @@
 
 Map::Map(std::string name, std::string layoutFile, sf::RenderWindow* w) {
     
+    //random number generation
+    srand(time(NULL)); //initialize random generation with time as seed
+    
     //set name and window reference
     this->name = name;
     this->w = w;
     
-    //load node
-    Node mn = SaveManager::loadMasterNode(layoutFile);
+    //load master data node for map
+    Node mn = SaveManager::loadMasterNode(layoutFile); //master node
     
-    //load width and height
-    int width = std::stoi(mn.getCwN("map_width").getV());
-    int height = std::stoi(mn.getCwN("map_height").getV());
+    //create map from loaded data
+    std::vector<CharTextureCombo> ctcs;
+    loadCTCs(&ctcs, mn.getCwN("map_chars")); //load CTCs
     
-    //fill statics with empty
-    for (int y = 0; y < height; y++) {
-        this->statics.push_back(std::vector<StaticTile*>());
-        for (int x = 0; x < width; x++) {
-            this->statics.at(y).push_back(nullptr);
-        }
+    for (CharTextureCombo ctc : ctcs) {
+        std::cout << ctc.character << ": " << std::endl;
+        for (std::string s : ctc.possibleTiles)
+            std::cout << "    " << s << "," << std::endl;
     }
     
-    //load the character types and layout
-    Node chars = mn.getCwN("map_chars");
-    Node layout = mn.getCwN("map_layout");
-    
-    //create layout
-    for (int y = 0; y < height; y++) {
-        std::string row = layout.getCwN("row " + std::to_string(y)).getCwN("layout").getV();
-        for (int x = 0; x < row.length(); x++) {
-            Node charRep("");
-            if (chars.getCwN(&charRep, std::string(1, row[x]))) {
-                this->statics.at(y).at(x) = new StaticTile(charRep.getV(), x, y);
-            }
-        }
-    }
+    this->loadBackground(mn.getCwN("background_layout"), &ctcs); //load background
+    this->loadWall(mn.getCwN("wall_layout"), &ctcs); //load wall
 }
 
 ///////////////////////////////////////////////////////////////////
@@ -72,12 +61,15 @@ Map::Map(Node stateInfo, sf::RenderWindow* w) {
 
 void Map::illustrate() {
     
-    //illustrate statics first
-    for (std::vector<StaticTile*> row : this->statics) { //each row
-        for (StaticTile* st: row) { //each item within that row
+    //illustrate background first
+    for (std::vector<StaticTile*> row : this->background) //each row
+        for (StaticTile* st: row) //each item within that row
             if (st != nullptr) st->illustrate(this->w); //illustrate
-        }
-    }
+    
+    //illustrate walls second
+    for (std::vector<StaticTile*> row : this->wall) //each row
+        for (StaticTile* st: row) //each item within that row
+            if (st != nullptr) st->illustrate(this->w); //illustrate
 }
 
 ///////////////////////////////////////////////////////////////////
@@ -87,6 +79,73 @@ void Map::illustrate() {
 
 Node Map::getStateInfo() {
     
+}
+
+///////////////////////////////////////////////////////////////////
+// Will attempt to convert the node (@n) into CTCs
+///////////////////////////////////////////////////////////////////
+
+void Map::loadCTCs(std::vector<CharTextureCombo>* ctcs, Node n) {
+    std::vector<Node> charReps = n.getC(); //get each representation node
+    for (Node charRep : charReps) { //loop through each representation node
+        CharTextureCombo ctc; //create ctc
+        ctc.character = charRep.getN()[0]; //set character
+        std::string tileList = charRep.getV(); //get possible tiles
+        remove_if(tileList.begin(), tileList.end(), isspace); //remove spaces
+        std::stringstream ss(tileList); //stream through the string
+        std::string possibleTile; //individual tile
+        while(std::getline(ss, possibleTile, ',')) ctc.possibleTiles.push_back(possibleTile); //split, add to vector
+        std::string correction = ctc.possibleTiles.at(ctc.possibleTiles.size() - 1); //correct additional characters at end
+        correction = correction.substr(0, correction.length() + 1 - ctc.possibleTiles.size());
+        ctc.possibleTiles.at(ctc.possibleTiles.size() - 1) = correction;
+        ctcs->push_back(ctc); //add to ctcs
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+// Loads the background layer of tiles from node (@n) using (@ctcs)
+///////////////////////////////////////////////////////////////////
+
+void Map::loadBackground(Node n, std::vector<CharTextureCombo>* ctcs) {
+    Node rowNode("");
+    for (int y = 0; n.getCwN(&rowNode, "row " + std::to_string(y)); y++) { //loop through each row
+        this->background.push_back(std::vector<StaticTile*>()); //add empty row
+        std::string row = rowNode.getCwN("layout").getV(); //get row
+        for (int x = 0; x < row.length(); x++) { //go through row
+            bool charRepFound = false;
+            for (CharTextureCombo ctc : *ctcs) { //search for character representation
+                if (ctc.character == row[x]) { //if found
+                    charRepFound = true; //flag as found
+                    int no = rand() % ctc.possibleTiles.size();
+                    this->background.at(y).push_back(new StaticTile(ctc.possibleTiles[no], x, y)); //add tile
+                }
+                if (!charRepFound) this->background.at(y).push_back(nullptr); //add empty tile
+            }
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+// Loads the wall layer of tiles from node (@n) using (@ctcs)
+///////////////////////////////////////////////////////////////////
+
+void Map::loadWall(Node n, std::vector<CharTextureCombo>* ctcs) {
+    Node rowNode("");
+    for (int y = 0; n.getCwN(&rowNode, "row " + std::to_string(y)); y++) { //loop through each row
+        this->wall.push_back(std::vector<StaticTile*>()); //add empty row
+        std::string row = rowNode.getCwN("layout").getV(); //get row
+        for (int x = 0; x < row.length(); x++) { //go through row
+            bool charRepFound = false;
+            for (CharTextureCombo ctc : *ctcs) { //search for character representation
+                if (ctc.character == row[x]) { //if found
+                    charRepFound = true; //flag as found
+                    int no = rand() % ctc.possibleTiles.size();
+                    this->wall.at(y).push_back(new StaticTile(ctc.possibleTiles[no], x, y)); //add tile
+                }
+                if (!charRepFound) this->wall.at(y).push_back(nullptr); //add empty tile
+            }
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////
--- wander/src/gameobject/Map.h
+++ wander/src/gameobject/Map.h
@@ -14,10 +14,26 @@
 // Includes
 
 #include <string>
+#include <sstream>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 #include <SFML/Graphics.hpp>
 #include "StaticTile.h"
 #include "SaveManager.h"
+
+///////////////////////////////////////////////////////////////////
+// CharTextureCombo struct
+// a piece of data which pairs a character to a set of
+// possible textures
+///////////////////////////////////////////////////////////////////
+
+struct CharTextureCombo { //define CharTextureCombo struct
+    
+    // Data
+    char character; //character representing the possible tiles
+    std::vector<std::string> possibleTiles; //the possible tiles
+};
 
 ///////////////////////////////////////////////////////////////////
 // Map Class
@@ -29,8 +45,8 @@ public:
     
     // Constructors
     Map() {}; //default constructor
-    Map(std::string name, std::string layoutFile, sf::RenderWindow* w); //for creating a new instance of map from template
-    Map(Node stateInfo, sf::RenderWindow* w); //for creating a new instance from a previously saved one
+    Map(std::string name, std::string layoutFile, sf::RenderWindow* w); //for creating a new instance of map
+    Map(Node stateInfo, sf::RenderWindow* w); //for creating an instance from a previously saved one
     
     // Utility Functions
     void illustrate(); //illustrates the map
@@ -41,11 +57,19 @@ public:
 private:
     
     // SFML Objects
-    sf::RenderWindow* w;
+    sf::RenderWindow* w; //reference to window
     
     // Data
     std::string name; //name of map
-    std::vector<std::vector<StaticTile*>> statics; //mapping of staticTiles
+    
+    // Map Layers
+    std::vector<std::vector<StaticTile*>> background; //mapping of background
+    std::vector<std::vector<StaticTile*>> wall; //mapping of walls
+    
+    // Map Loading Methods
+    void loadCTCs(std::vector<CharTextureCombo>* ctcs, Node n); //loads CTCs
+    void loadBackground(Node n, std::vector<CharTextureCombo>* ctcs); //loads background layer of tiles
+    void loadWall(Node n, std::vector<CharTextureCombo>* ctcs); //loads background layer of walls
     
 };
 
